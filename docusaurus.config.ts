982664168db5957b84719049030d3869b00a42a9
Gregory Kelly <git@gregk.ca>
2026-01-28 17:30:08 -0500

    Update branding to Mavericks

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Shop Site',
+  tagline: 'Machine Mavericks Shop resources',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -72,10 +72,11 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
-      respectPrefersColorScheme: true,
+      respectPrefersColorScheme: false,
+      defaultMode: 'light'
     },
     navbar: {
-      title: 'My Site',
+      title: 'Shop Site',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
